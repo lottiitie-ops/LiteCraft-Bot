fd6f5d0b88d96dffba2f453c6a0ff5aa61f5dbe0
lottie <lottiitie@gmail.com>
2026-03-03 16:32:40 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"🔥 Bot conectado como {bot.user}")
+
+bot.run(os.getenv("TOKEN"))
